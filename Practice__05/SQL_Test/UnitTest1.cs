12b0b60c02acabad8f6c0187e90fd72994197799
Liza Tsaturyan <lt291@inlumine.ual.es>
2024-05-25 15:23:23 -0700

    testing c (1st test)

--- Practice__05/SQL_Test/UnitTest1.cs
+++ Practice__05/SQL_Test/UnitTest1.cs
@@ -1,21 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Data;
 using System.Data.SqlClient;
-using System.Reflection;
 
 namespace SQL_Test
 {
     [TestClass]
     public class UnitTest1
     {
-        public static string ExecutionDirectoryPathName()
-        {
-            var dirPath = Assembly.GetExecutingAssembly().Location;
-            dirPath = Path.GetDirectoryName(dirPath);
-            return dirPath;
-        }
-
         [TestMethod]
         public void SqlServerFile_TestConnection()
         {
@@ -37,35 +28,6 @@ namespace SQL_Test
             catch (Exception ex)
             {
                 Assert.Fail($"Exception: {ex.Message}");
-            }
-        }
-
-        [TestMethod]
-        public void SqlServerFile_InsertRecordTest()
-        {
-            string connectionString = @"data source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Laura\source\repos\2022-pss-yulhev2\Practice__05\Practice__05\AutentificacionDB.mdf;Integrated Security=True;";
-            int result;
-
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    string name = "N_" + Guid.NewGuid().ToString();
-                    SqlCommand cmd = conn.CreateCommand();
-                    cmd.CommandText = "Insert INTO Authentications (Name, Password, Category, IsValid) " +
-                                      "values (@newName, @newPalabraPaso, @newCategoria, @newEsValido)";
-                    cmd.Parameters.Add("@newName", SqlDbType.NVarChar, 20).Value = name;
-                    cmd.Parameters.Add("@newPalabraPaso", SqlDbType.NVarChar, 20).Value = "Stepword";
-                    cmd.Parameters.Add("@newCategoria", SqlDbType.NVarChar, 20).Value = "Category";
-                    cmd.Parameters.Add("@newEsValido", SqlDbType.Bit).Value = true;
-                    result = cmd.ExecuteNonQuery();
-                    if (result != 1) throw new Exception("Error inserting a new record.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail("The exception was not expected: " + ex.Message);
             }
         }
     }
